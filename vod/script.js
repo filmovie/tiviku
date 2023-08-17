862da8dbcd56a0a35baf0791ccadedb6e53908f7
tiviku <105354467+filmovie@users.noreply.github.com>
2023-08-17 18:39:26 +0700

    Add files via upload

--- vod/script.js
+++ vod/script.js
@@ -1,7 +1,21 @@
-// Change the second argument to your options:
-// https://github.com/sampotts/plyr/#options
-          var player = new Plyr('.container video', {
-            muted: false,
-            volume: 1,
-            controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
-          });
+var requestFullscreen = function (element) {
+	if (element.requestFullscreen) {
+		element.requestFullscreen();
+	} else if (element.webkitRequestFullscreen) {
+		element.webkitRequestFullscreen();
+	} else if (element.mozRequestFullScreen) {
+		element.mozRequestFullScreen();
+	} else if (element.msRequestFullscreen) {
+		element.msRequestFullscreen();
+	} else {
+		console.log('Fullscreen API is not supported.');
+	}
+};
+
+var fullscreenVideo = document.getElementById('video-button');
+var video = document.getElementById('video');
+
+fullscreenVideo.addEventListener('click', function(e) {
+	e.preventDefault();
+	requestFullscreen(video);
+});
