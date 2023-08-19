b578bf439f974749ba70b90eb3a977fa3844d2f2
tiviku <105354467+filmovie@users.noreply.github.com>
2023-08-19 21:58:09 +0700

    Add files via upload

--- tv/script.js
+++ tv/script.js
@@ -1,135 +1,152 @@
-var Player = (function(window, videojs) {
-  var rotateBtn = videojs.extend(videojs.getComponent("Button"), {
-    constructor: function(player) {
-      videojs.getComponent("Button").apply(this, arguments);
-      this.controlText("Rotate");
-      this.rotate = 0;
-      this.player = player;
+'use strict';
+
+var videoPlayer = window.videoPlayer || {};
+
+(function () {  
+  var options = {
+    autoplay: true, 
+    controls: true, 
+    muted: false, 
+    // fluid: true,
+    playbackRates: [1, 1.5, 2, 2.5],
+    inactivityTimeout: 0, // 0: user will never be considered inactive
+    width: 500
+  };
+  var url = 'https://s3-us-west-1.amazonaws.com/';
+  var queryString = '?path=joepc';  // replace with 'location.search'
+  
+  var params = {};
+  if (queryString) { // parameterize the query string (Credit: https://stackoverflow.com/a/21152762/2213679)
+    queryString.substr(1).split('&').forEach(function(item) {
+      var s = item.split('='), k = s[0], v = s[1] && decodeURIComponent(s[1]);
+      (params[k] = params[k] || []).push(v);
+    });  
+  }
+  
+  var baseUrl = url + params['path'] + '/';
+  var types = {mp4:'video/mp4',webm:'video/webm', m3u: 'application/x-mpegURL'};
+
+  var vPlaylistMax = 50;   // Look for videos up to Max
+  var vPlaylist = [];      // video playlist
+  var vExtension = '.mp4'; // extension (assume same for a series)
+  var vUrl;                // URL string to be built
+
+  for (var i=1; i <= vPlaylistMax; i++) {
+    vUrl = baseUrl+i+vExtension;
+    if ( fileExists(vUrl) ) {
+      vPlaylist.push({sources: [{src: vUrl, type: types.mp4}]});
+    } else { break; } // exit loop on first missing file
+  }
+  
+  function fileExists(url) {
+    var http = new XMLHttpRequest();
+    // (method,url,async) 
+    // 'HEAD' -- same as GET request, but without response body.
+    // async:false -- send() does not return until response is received.
+    http.open('HEAD',url,false);
+    http.send();
+    return (http.status==200);    
+  }
+
+  var rate = 1; // playback starting rate (1x)
+  var player = videojs('playerOne', options);
+  
+  player.overlay({
+    // content: 'Default overlay content',
+    debug: false,
+    overlays: [{
+      content: '<h2>Playing <span id="playlist-current-number">1</span> of ' +vPlaylist.length+ '</h2>', //'The video is playing!',
+
+      start: 'play',
+      end: 5,
+      align: 'top-left',
+      showBackground: true
+    }]
+  });
+  player.ready(function() {
+    this.playlist(vPlaylist);
+    this.playlist.autoadvance(1); // Autoplay next video (0-second lead)
+    this.playlist.repeat(true);   // Allow skipping back to first video
+    this.play();
+  });
+
+  // Get playback rate, since it doesn't persist from video to video
+  player.on('beforeplaylistitem', function() {
+    rate = this.playbackRate(); 
+  });
+  
+  // Set playback rate
+  player.on('playlistitem', function() {
+    var currentNumber = document.getElementById('playlist-current-number');
+    currentNumber.innerText = this.playlist.currentItem() + 1;
+
+    this.playbackRate(rate); });
+
+  var buttonComponent = videojs.getComponent('Button');
+  
+  var prevButton = videojs.extend(buttonComponent, {    
+    constructor: function() {
+      buttonComponent.apply(this, arguments);
+      this.addClass('vjs-icon-previous-item');
+      this.controlText('Previous');
     },
-    handleClick: function() {
-      this.removeClass("rotate-" + this.rotate);
-      this.rotate += 90;
-      zoom = this.rotate % 180 === 0 ? 1 : 0.5;
-      this.rotate = this.rotate % 360 || 0;
-      this.player.zoomrotate({ rotate: this.rotate, zoom: zoom });
-      this.addClass("rotate-" + this.rotate);
-    },
-    buildCSSClass: function() {
-      return "rotate-0 vjs-control vjs-button";
+    handleClick: function(e) {
+      player.playlist.previous();
     }
   });
-  var zoomrotatePlugin = function(settings) {
-    var defaults, extend;
-    defaults = {
-      zoom: 1,
-      rotate: 0
-    };
-    extend = function() {
-      var args, target, i, object, property;
-      args = Array.prototype.slice.call(arguments);
-      target = args.shift() || {};
-      for (i in args) {
-        object = args[i];
-        for (property in object) {
-          if (object.hasOwnProperty(property)) {
-            if (typeof object[property] === "object") {
-              target[property] = extend(target[property], object[property]);
-            } else {
-              target[property] = object[property];
-            }
-          }
-        }
-      }
-      return target;
-    };
 
-    var options, player, video, poster;
-    options = extend(defaults, settings);
-
-    player = this.el();
-    video = this.el().getElementsByTagName("video")[0];
-    poster = this.el().getElementsByTagName("div")[1];
-    
-    var properties = [
-        "transform",
-        "WebkitTransform",
-        "MozTransform",
-        "msTransform",
-        "OTransform"
-      ],
-      prop = properties[0];
-
-    var i, j;
-    
-    for (i = 0, j = properties.length; i < j; i++) {
-      if (typeof player.style[properties[i]] !== "undefined") {
-        prop = properties[i];
-        break;
-      }
+  var nextButton = videojs.extend(buttonComponent, {    
+    constructor: function() {
+      buttonComponent.apply(this, arguments);
+      this.addClass('vjs-icon-next-item');
+      this.controlText('Next');
+    },
+    handleClick: function(e) {
+      player.playlist.next();
+      // this.controlText('Next (part 3)');
     }
+  });
 
-    player.style.overflow = "hidden";
-    video.style[prop] =
-      "scale(" + options.zoom + ") rotate(" + options.rotate + "deg)";
-    poster.style[prop] =
-      "scale(" + options.zoom + ") rotate(" + options.rotate + "deg)";
-    if (options.debug) console.log("zoomrotate: Register end");
-  };
-  var createPlayer = function() {
-    var vid = document.querySelector("#censor-player");
-    vid.className = "video-js vjs-default-skin vjs-big-play-centered";
-    var player = videojs(vid, {
-      controls: true,
-      playbackRates: [1, 1.2, 1.5, 1.8, 2],
-      controlBar: {
-        audioTrackButton: false,
-        subsCapsButton: false
-      }
-    });
-    player.src({
-      src: vid.src,
-      type: vid.type || "application/x-mpegURL"
-    });
-    player.width(vid.style.width || 450);
-    player.height(vid.style.height || 300);
-    player.getChild("controlBar").addChild("rotateButton", {});
-    var seekBar = player.controlBar.progressControl.seekBar;
-    window.addEventListener(
-      "keydown",
-      function(event) {
-        switch (event.keyCode) {
-          case 32: //Space
-            if (player.paused()) {
-              player.play();
-            } else {
-              player.pause();
-            }
-            break;
-          case 39: //ArrowRight
-            var cur = player.currentTime();
-            player.currentTime(cur + 5);
-            break;
-          case 37: //ArrowLeft
-            var cur = player.currentTime();
-            player.currentTime(cur - 5);
-            break;
-        }
-      },
-      false
-    );
-    window.onkeydown = function(e) { 
-      return !(e.keyCode == 32);
-    };
-  };
-  var init = function() {
-    videojs.registerComponent("RotateButton", rotateBtn);
-    videojs.registerPlugin("zoomrotate", zoomrotatePlugin);
-    createPlayer();
-  };
+  videojs.registerComponent('prevButton', prevButton);
+  videojs.registerComponent('nextButton', nextButton);
 
-  return {
-    init: init
-  };
-})(window, videojs);
+  player.getChild('controlBar').addChild('prevButton', {}, 0);
+  player.getChild('controlBar').addChild('nextButton', {}, 2);
+})(videoPlayer);
 
-Player.init();
+
+  
+  // var vPlaylist = [
+  // {sources: [{src: 'https://vjs.zencdn.net/v/oceans.mp4', 
+  //             type: types.mp4},
+  //            {src: 'https://vjs.zencdn.net/v/oceans.webm', 
+  //             type: types.webm }]},
+  // {sources: [{src: 'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8', 
+  //             type: types.m3u}]},
+  // {sources: [{src: 'http://d2zihajmogu5jn.cloudfront.net/big-buck-bunny/master.m3u8',
+  //             type: types.m3u}]}];
+
+//   var buttonComponent = videojs.getComponent('Button');
+//   var buttons = ['vprevious','vnext'];
+//   var buttonObj;
+  
+//   for (var i=0; i < buttons.length; i++) {
+//     var button = buttons[i];
+//     buttonObj = videojs.extend(buttonComponent, {    
+//       constructor: function() {
+//         buttonComponent.apply(this, arguments);
+//         this.addClass('icon-' +button);
+//         this.controlText(button);
+//       },
+//       handleClick: function(e) {
+//         switch(button) {
+//           case 'vprevious': player.playlist.previous(); console.log(button); break;
+//           case 'vnext': player.playlist.next(); console.log(button);  break;
+//         }
+//       }
+//     });
+
+//     videojs.registerComponent(button,buttonObj);
+//   }
+//   player.getChild('controlBar').addChild('vprevious', {},0); 
+//   player.getChild('controlBar').addChild('vnext',{},2);
