5b2f0ad3229e4dcc7089986acb493e1723c7dac7
tiviku <105354467+filmovie@users.noreply.github.com>
2023-09-11 16:29:22 +0700

    Add files via upload

---
+++ js/ghost.at.school.(2000).dubbing.js
@@ -0,0 +1,84 @@
+let scrollmenu = document.createElement('div');
+scrollmenu.align ="center";
+scrollmenu.className = "scrollmenu";
+document.body.appendChild(scrollmenu);
+
+let a1 = document.createElement('span');
+a1.innerHTML = "<a class='film' href=' vod/1.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost1.png?raw=true ' alt=' eps.1 '></button></a>";
+scrollmenu.appendChild(a1);
+
+let a2 = document.createElement('span');
+a2.innerHTML = "<a class='film' href=' vod/2.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost2.png?raw=true ' alt=' eps.2 '></button></a>";
+scrollmenu.appendChild(a2);
+
+let a3 = document.createElement('span');
+a3.innerHTML = "<a class='film' href=' vod/3.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost3.png?raw=true ' alt=' eps.3 '></button></a>";
+scrollmenu.appendChild(a3);
+
+let a4 = document.createElement('span');
+a4.innerHTML = "<a class='film' href=' vod/4.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost4.png?raw=true ' alt=' eps.4 '></button></a>";
+scrollmenu.appendChild(a4);
+
+let a5 = document.createElement('span');
+a5.innerHTML = "<a class='film' href=' vod/5.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost5.png?raw=true ' alt=' eps.5 '></button></a>";
+scrollmenu.appendChild(a5);
+
+let a6 = document.createElement('span');
+a6.innerHTML = "<a class='film' href=' vod/6.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost6.png?raw=true ' alt=' eps.6 '></button></a>";
+scrollmenu.appendChild(a6);
+
+let a7 = document.createElement('span');
+a7.innerHTML = "<a class='film' href=' vod/7.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost7.png?raw=true ' alt=' eps.7 '></button></a>";
+scrollmenu.appendChild(a7);
+
+let a8 = document.createElement('span');
+a8.innerHTML = "<a class='film' href=' vod/8.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost8.png?raw=true ' alt=' eps.8 '></button></a>";
+scrollmenu.appendChild(a8);
+
+let a9 = document.createElement('span');
+a9.innerHTML = "<a class='film' href=' vod/9.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost9.png?raw=true ' alt=' eps.9 '></button></a>";
+scrollmenu.appendChild(a9);
+
+let a10 = document.createElement('span');
+a10.innerHTML = "<a class='film' href=' vod/10.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost10.png?raw=true ' alt=' eps.10 '></button></a>";
+scrollmenu.appendChild(a10);
+
+let a11 = document.createElement('span');
+a11.innerHTML = "<a class='film' href=' vod/11.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost11.png?raw=true ' alt=' eps.11 '></button></a>";
+scrollmenu.appendChild(a11);
+
+let a12 = document.createElement('span');
+a12.innerHTML = "<a class='film' href=' vod/12.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost12.png?raw=true ' alt=' eps.12 '></button></a>";
+scrollmenu.appendChild(a12);
+
+let a13 = document.createElement('span');
+a13.innerHTML = "<a class='film' href=' vod/13.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost13.png?raw=true ' alt=' eps.13 '></button></a>";
+scrollmenu.appendChild(a13);
+
+let a14 = document.createElement('span');
+a14.innerHTML = "<a class='film' href=' vod/14.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost14.png?raw=true ' alt=' eps.14 '></button></a>";
+scrollmenu.appendChild(a14);
+
+let a15 = document.createElement('span');
+a15.innerHTML = "<a class='film' href=' vod/15.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost15.png?raw=true ' alt=' eps.15 '></button></a>";
+scrollmenu.appendChild(a15);
+
+let a16 = document.createElement('span');
+a16.innerHTML = "<a class='film' href=' vod/16.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost16.png?raw=true ' alt=' eps.16 '></button></a>";
+scrollmenu.appendChild(a16);
+
+let a17 = document.createElement('span');
+a17.innerHTML = "<a class='film' href=' vod/17.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost17.png?raw=true ' alt=' eps.17 '></button></a>";
+scrollmenu.appendChild(a17);
+
+let a18 = document.createElement('span');
+a18.innerHTML = "<a class='film' href=' vod/18.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost18.png?raw=true ' alt=' eps.18 '></button></a>";
+scrollmenu.appendChild(a18);
+
+let a19 = document.createElement('span');
+a19.innerHTML = "<a class='film' href=' vod/19.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost19.png?raw=true ' alt=' eps.19 '></button></a>";
+scrollmenu.appendChild(a19);
+
+let a20 = document.createElement('span');
+a20.innerHTML = "<a class='film' href=' vod/20.ghost.at.school.(2000).dubbing.html '><button><img class='gfilm' src=' https://github.com/filmovie/by/blob/main/pTq/ghost20.png?raw=true ' alt=' eps.20 '></button></a>";
+scrollmenu.appendChild(a20);
