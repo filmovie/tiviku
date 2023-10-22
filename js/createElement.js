b8a97eeec0d92441935b6bfd7b591b79fc0181f0
tiviku <105354467+filmovie@users.noreply.github.com>
2023-10-22 14:57:03 +0700

    Add files via upload

--- js/createElement.js
+++ js/createElement.js
@@ -6,6 +6,18 @@ document.body.appendChild(scrollmenu);
 /*
 
 */
+let a8 = document.createElement('span');
+a8.innerHTML = "<a class='film' href=' vod/20th.Century.Boys.3.Redemption.(2009).html '><button><img class='gfilm' src=' https://www.themoviedb.org/t/p/w188_and_h282_bestv2/zf5wTUqciMcSEUjPF5E9O8fBNo9.jpg ' alt=' 20th.Century.Boys.3.Redemption.(2009) '></button></a>";
+scrollmenu.appendChild(a8);
+
+let a7 = document.createElement('span');
+a7.innerHTML = "<a class='film' href=' vod/5cm.(2012).html '><button><img class='gfilm' src=' https://www.themoviedb.org/t/p/w220_and_h330_face/y43O9tj6yo4h1shx58YGBsbjme4.jpg ' alt=' 5cm.(2012) '></button></a>";
+scrollmenu.appendChild(a7);
+
+let a6 = document.createElement('span');
+a6.innerHTML = "<a class='film' href=' vod/Doraemon.Nobita.and.the.New.Steel.Troops.Winged.Angels.(2011).dubbing.html '><button><img class='gfilm' src=' https://www.themoviedb.org/t/p/w188_and_h282_bestv2/jbszRvlAtGGHZK4qOAREHChg2P7.jpg ' alt=' Doraemon.Nobita.and.the.New.Steel.Troops.Winged.Angels.(2011).dubbing '></button></a>";
+scrollmenu.appendChild(a6);
+
 let a5 = document.createElement('span');
 a5.innerHTML = "<a class='film' href=' vod/antri.dong.(1990).doyok.kadir.html '><button><img class='gfilm' src=' https://www.themoviedb.org/t/p/w220_and_h330_face/hCQZtiv4M4JggJ17yb8VlJgJJwx.jpg ' alt=' antri.dong.(1990).doyok.kadir '></button></a>";
 scrollmenu.appendChild(a5);
