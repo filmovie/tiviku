e5e8b90f2d8e32f03caff4a520c6b0e279383606
tiviku <105354467+filmovie@users.noreply.github.com>
2023-10-22 09:38:53 +0700

    Add files via upload

---
+++ js/fvod.js
@@ -0,0 +1,90 @@
+let script = document.createElement('script');
+script.src = "../js/time.js";
+document.body.appendChild(script);
+
+var ggg = document.querySelector('.tiviku');
+
+let tiviku = document.createElement('img');
+tiviku.src="../tiviku.gif";
+tiviku.className="tiv";
+ggg.appendChild(tiviku);
+
+var ply = document.getElementById("wpo");
+ply.src ='../iklan1.mp4';
+
+var vide = document.getElementById("myvideo");
+vide.src ='../iklan1.mp4';
+
+var vidy = document.querySelector(".vid1");
+
+var vidd3 = document.querySelector(".vid3")
+
+setTimeout(function(){
+				vide.pause()
+},1200000);
+setTimeout(function(){
+				vide.play()
+},1208000);
+
+setTimeout(function(){
+				vidy.classList.add("vid3")
+				vidy.play()
+},1200000);
+
+setTimeout(function(){
+				vidy.classList.remove("vid3")
+				vidy.pause()
+},1208000);
+
+
+const video = document.querySelector(".video");
+const toggleButton = document.querySelector(".toggleButton");
+const progress = document.querySelector(".progress");
+const progressBar = document.querySelector(".progress__filled");
+const sliders = document.querySelectorAll(".controls__slider");
+const skipBtns = document.querySelectorAll("[data-skip]");
+
+function togglePlay() {
+  if (video.paused || video.ended) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+function updateToggleButton() {
+  toggleButton.innerHTML = video.paused ? "<b>▶</b>" : "❚ ❚";
+}
+
+function handleProgress() {
+  const progressPercentage = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${progressPercentage}%`;
+}
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function handleSliderUpdate() {
+  video[this.name] = this.value;
+}
+function handleSkip() {
+  video.currentTime += +this.dataset.skip;
+}
+toggleButton.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateToggleButton);
+video.addEventListener("pause", updateToggleButton);
+video.addEventListener("timeupdate", handleProgress);
+sliders.forEach((slider) => {
+  slider.addEventListener("change", handleSliderUpdate);
+});
+skipBtns.forEach((btn) => {
+  btn.addEventListener("click", handleSkip);
+});
+
+sliders.forEach((slider) => {
+  slider.addEventListener("change", handleSliderUpdate);
+});
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space") togglePlay();
+});
