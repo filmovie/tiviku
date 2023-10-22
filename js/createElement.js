b7eb93f21046d424fca632bd5b34bbfdbca48d43
tiviku <105354467+filmovie@users.noreply.github.com>
2023-10-22 12:06:33 +0700

    Add files via upload

--- js/createElement.js
+++ js/createElement.js
@@ -6,9 +6,22 @@ document.body.appendChild(scrollmenu);
 /*
 
 */
+let a5 = document.createElement('span');
+a5.innerHTML = "<a class='film' href=' vod/antri.dong.(1990).doyok.kadir.html '><button><img class='gfilm' src=' https://www.themoviedb.org/t/p/w220_and_h330_face/hCQZtiv4M4JggJ17yb8VlJgJJwx.jpg ' alt=' antri.dong.(1990).doyok.kadir '></button></a>";
+scrollmenu.appendChild(a5);
+
+let a4 = document.createElement('span');
+a4.innerHTML = "<a class='film' href=' vod/Air.Terjun.Pengantin.(2009).html '><button><img class='gfilm' src=' https://www.themoviedb.org/t/p/w220_and_h330_face/rGFFl5OXkhpHuS7zOJmZqxfgqgF.jpg ' alt=' Air.Terjun.Pengantin.(2009) '></button></a>";
+scrollmenu.appendChild(a4);
+
+let a3 = document.createElement('span');
+a3.innerHTML = "<a class='film' href=' vod/A.Savannah.Haunting.(2022).html '><button><img class='gfilm' src=' https://www.themoviedb.org/t/p/w220_and_h330_face/fpxmnwY9mQk05JVya6lsbvUm3mW.jpg ' alt=' A.Savannah.Haunting.(2022) '></button></a>";
+scrollmenu.appendChild(a3);
+
 let a2 = document.createElement('span');
 a2.innerHTML = "<a class='film' href=' vod/2.Fast.2.Furious.(2003).html '><button><img class='gfilm' src=' https://www.themoviedb.org/t/p/w220_and_h330_face/6nDZExrDKIXvSAghsFKVFRVJuSf.jpg ' alt=' 2.Fast.2.Furious.(2003) '></button></a>";
 scrollmenu.appendChild(a2);
+
 let a1 = document.createElement('span');
 a1.innerHTML = "<a class='film' href=' vod/The.Raid_Redemption.(2011).html '><button><img class='gfilm' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIJJdrTCfoX_2XWFkhW6-K-FuNDCZMvIzDng&usqp=CAU' alt=' The.Raid_Redemption.(2011) '></button></a>";
 scrollmenu.appendChild(a1);
