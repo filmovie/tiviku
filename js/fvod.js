79205fa6baf1c7838b8e8faaf67bfa98f4bbae72
tiviku <105354467+filmovie@users.noreply.github.com>
2023-11-10 21:29:13 +0700

    Add files via upload

--- js/fvod.js
+++ js/fvod.js
@@ -11,9 +11,11 @@ ggg.appendChild(tiviku);
 
 var ply = document.getElementById("wpo");
 ply.src ='../iklan1.mp4';
+ply.preload = "none";
 
 var vide = document.getElementById("myvideo");
 vide.src ='https://dl.dropbox.com/scl/fi/p57infaytbffqiofwt7c4/1p.mp4?rlkey=8pexarsdkxi95kn6jbm2at4sv&dl=0';
+vide.preload = "none";
 
 var vidy = document.querySelector(".vid1");
 
