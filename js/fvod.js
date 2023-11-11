4036a4f0e54b2bdc284271365a7d109e44163a94
tiviku <105354467+filmovie@users.noreply.github.com>
2023-11-11 16:38:05 +0700

    Update fvod.js

--- js/fvod.js
+++ js/fvod.js
@@ -11,11 +11,11 @@ ggg.appendChild(tiviku);
 
 var ply = document.getElementById("wpo");
 ply.src ='../iklan1.mp4';
-ply.preload = "none";
+ply.preload = "metadata";
 
 var vide = document.getElementById("myvideo");
 vide.src ='https://dl.dropbox.com/scl/fi/p57infaytbffqiofwt7c4/1p.mp4?rlkey=8pexarsdkxi95kn6jbm2at4sv&dl=0';
-vide.preload = "none";
+vide.preload = "metadata";
 
 var vidy = document.querySelector(".vid1");
 
