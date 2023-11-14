611858ceeedcc772593e757d8e1a5e3805d91251
tiviku <105354467+filmovie@users.noreply.github.com>
2023-11-14 07:02:48 +0700

    Add files via upload

--- js/Squid.Game.(2021).Dubbing.js
+++ js/Squid.Game.(2021).Dubbing.js
@@ -9,13 +9,17 @@ var goe = document.getElementById("goe");
 goe.remove()
 
 let form = document.createElement("form");
-form.innerHTML ="<div class='container'><div class='left'><input type='text'name='' id='cari' onkeyup='prosesMenu()' placeholder='search'></div></div>";
+form.innerHTML ="<div class='container'><div class='left'><input type='text'name='' id='cari' onkeyup='prosesMenu()' placeholder='search'><br><br></div></div>";
 frm.appendChild(form);
 
 let a1 = document.createElement('span');
-a1.innerHTML = "<a class='film' href=' fvod/1.Squid.Game.(2021).Dubbingv.html '><button><img class='gfilm' src=' gambar/squid1.jpg ' alt=' eps.1 '></button></a>";
+a1.innerHTML = "<a class='film' href=' fvod/1.Squid.Game.(2021).Dubbingv.html '><button><img class='gfilm' src=' gambar/squid1.jpg ' alt=' eps.01 '></button></a>";
 scrollmenu.appendChild(a1);
 
 let a2 = document.createElement('span');
-a2.innerHTML = "<a class='film' href=' fvod/2.Squid.Game.(2021).Dubbingv.html '><button><img class='gfilm' src=' gambar/squid2.jpg ' alt=' eps.2 '></button></a>";
+a2.innerHTML = "<a class='film' href=' fvod/2.Squid.Game.(2021).Dubbingv.html '><button><img class='gfilm' src=' gambar/squid2.jpg ' alt=' eps.02 '></button></a>";
 scrollmenu.appendChild(a2);
+
+let a3 = document.createElement('span');
+a3.innerHTML = "<a class='film' href=' fvod/3.Squid.Game.(2021).Dubbingv.html '><button><img class='gfilm' src=' gambar/squid3.jpg ' alt=' eps.03 '></button></a>";
+scrollmenu.appendChild(a3);
