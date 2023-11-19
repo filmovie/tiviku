464e4df8ef19b97fbbc945276c3cf5a39b39a229
tiviku <105354467+filmovie@users.noreply.github.com>
2023-11-19 08:27:41 +0700

    Add files via upload

--- js/bluelock.(2022).js
+++ js/bluelock.(2022).js
@@ -12,10 +12,10 @@ let form = document.createElement("form");
 form.innerHTML ="<div class='container'><div class='left'><input type='text'name='' id='cari' onkeyup='prosesMenu()' placeholder='search'><br><br></div></div>";
 frm.appendChild(form);
 
-let a2 = document.createElement('span');
-a2.innerHTML = "<a class='film' href=' fvod/2.bluelock.(2022)v.html '><button><img class='gfilm' src=' gambar/block2.jpg ' alt=' eps.02 '></button></a>";
-scrollmenu.appendChild(a2);
-
 let a1 = document.createElement('span');
 a1.innerHTML = "<a class='film' href=' fvod/1.bluelock.(2022)v.html '><button><img class='gfilm' src=' gambar/block1.jpg ' alt=' eps.01 '></button></a>";
 scrollmenu.appendChild(a1);
+
+let a2 = document.createElement('span');
+a2.innerHTML = "<a class='film' href=' fvod/2.bluelock.(2022)v.html '><button><img class='gfilm' src=' gambar/block2.jpg ' alt=' eps.02 '></button></a>";
+scrollmenu.appendChild(a2);
