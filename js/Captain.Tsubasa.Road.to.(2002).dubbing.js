246498819f1d8f223edfdeefc78c4b797bd9ad8f
tiviku <105354467+filmovie@users.noreply.github.com>
2023-11-26 06:06:57 +0700

    Add files via upload

---
+++ js/Captain.Tsubasa.Road.to.(2002).dubbing.js
@@ -0,0 +1,33 @@
+let scrollmenu = document.createElement('div');
+scrollmenu.align ="center";
+scrollmenu.className = "scrollmenu";
+document.body.appendChild(scrollmenu);
+
+var frm = document.getElementById("gtu");
+var goe = document.getElementById("goe");
+
+goe.remove()
+
+let form = document.createElement("form");
+form.innerHTML ="<div class='container'><div class='left'><input type='text'name='' id='cari' onkeyup='prosesMenu()' placeholder='search'><br><br></div></div>";
+frm.appendChild(form);
+
+let a1 = document.createElement('span');
+a1.innerHTML = "<a class='film' href=' fvod/1.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button><h2 class='gfil'>eps.01<h2></button></a>";
+scrollmenu.appendChild(a1);
+
+let a2 = document.createElement('span');
+a2.innerHTML = "<a class='film' href=' fvod/2.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button><h2 class='gfil'>eps.02<h2></button></a>";
+scrollmenu.appendChild(a2);
+
+let a3 = document.createElement('span');
+a3.innerHTML = "<a class='film' href=' fvod/3.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button><h2 class='gfil'>eps.03<h2></button></a>";
+scrollmenu.appendChild(a3);
+
+let a4 = document.createElement('span');
+a4.innerHTML = "<a class='film' href=' fvod/4.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button><h2 class='gfil'>eps.04<h2></button></a>";
+scrollmenu.appendChild(a4);
+
+let a5 = document.createElement('span');
+a5.innerHTML = "<a class='film' href=' fvod/5.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button><h2 class='gfil'>eps.05<h2></button></a>";
+scrollmenu.appendChild(a5);
