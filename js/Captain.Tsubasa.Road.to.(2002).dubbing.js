891847947b446941c81db5ea3f7dd7fe1c5af9c3
tiviku <105354467+filmovie@users.noreply.github.com>
2023-11-26 08:29:15 +0700

    Add files via upload

--- js/Captain.Tsubasa.Road.to.(2002).dubbing.js
+++ js/Captain.Tsubasa.Road.to.(2002).dubbing.js
@@ -13,21 +13,21 @@ form.innerHTML ="<div class='container'><div class='left'><input type='text'name
 frm.appendChild(form);
 
 let a1 = document.createElement('span');
-a1.innerHTML = "<a class='film' href=' fvod/1.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button class='gf'><p class='gfil'><b>eps.01</b></p></button></a>";
+a1.innerHTML = "<a class='film' href=' fvod/1.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button class='gf'><p><b>eps.01</b></p></button></a>";
 scrollmenu.appendChild(a1);
 
 let a2 = document.createElement('span');
-a2.innerHTML = "<a class='film' href=' fvod/2.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button class='gf'><p class='gfil'><b>eps.02</b></p></button></a>";
+a2.innerHTML = "<a class='film' href=' fvod/2.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button class='gf'><p><b>eps.02</b></p></button></a>";
 scrollmenu.appendChild(a2);
 
 let a3 = document.createElement('span');
-a3.innerHTML = "<a class='film' href=' fvod/3.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button class='gf'><p class='gfil'><b>eps.03</b></p></button></a>";
+a3.innerHTML = "<a class='film' href=' fvod/3.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button class='gf'><p><b>eps.03</b></p></button></a>";
 scrollmenu.appendChild(a3);
 
 let a4 = document.createElement('span');
-a4.innerHTML = "<a class='film' href=' fvod/4.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button class='gf'><p class='gfil'><b>eps.04</b></p></button></a>";
+a4.innerHTML = "<a class='film' href=' fvod/4.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button class='gf'><p><b>eps.04</b></p></button></a>";
 scrollmenu.appendChild(a4);
 
 let a5 = document.createElement('span');
-a5.innerHTML = "<a class='film' href=' fvod/5.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button class='gf'><p class='gfil'><b>eps.05</b></p></button></a>";
+a5.innerHTML = "<a class='film' href=' fvod/5.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button class='gf'><p><b>eps.05</b></p></button></a>";
 scrollmenu.appendChild(a5);
