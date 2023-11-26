20e60237537aab8ab193788e3174d7ea55a31793
tiviku <105354467+filmovie@users.noreply.github.com>
2023-11-26 14:48:43 +0700

    Add files via upload

--- vodi/js.js
+++ vodi/js.js
@@ -2,7 +2,7 @@ var vdi1 = document.getElementById("vdi1");
 
 var vide = document.getElementById("myvideo");
 vide.src ='https://dl.dropbox.com/scl/fi/p57infaytbffqiofwt7c4/1p.mp4?rlkey=8pexarsdkxi95kn6jbm2at4sv&dl=0';
-vide.preload = "metadata";
+vide.preload = "none";
 
 let tiviku = document.createElement('img');
 tiviku.src="https://github.com/filmovie/tiviku/blob/main/gambar/tiviku.png?raw=true";
