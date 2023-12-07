bf39d0f9242b020ce23563ea457a7cf1c3a55c54
tiviku <105354467+filmovie@users.noreply.github.com>
2023-12-07 07:07:04 +0700

    Add files via upload

--- js/Avatar.The.Legend.Of.Aang.(2005).dubbing.js
+++ js/Avatar.The.Legend.Of.Aang.(2005).dubbing.js
@@ -15,3 +15,7 @@ frm.appendChild(form);
 let a1 = document.createElement('span');
 a1.innerHTML = "<a class='film' href=' fvod/1.Avatar.The.Legend.Of.Aang.(2005).dubbing.html '><button class='gf'><p><b>eps.01</b></p></button></a>";
 scrollmenu.appendChild(a1);
+
+let a2 = document.createElement('span');
+a2.innerHTML = "<a class='film' href=' fvod/2.Avatar.The.Legend.Of.Aang.(2005).dubbing.html '><button class='gf'><p><b>eps.02</b></p></button></a>";
+scrollmenu.appendChild(a2);
